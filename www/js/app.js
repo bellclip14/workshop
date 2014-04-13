825fc6a72aea423f229ee6b0ff5f62102cbb9291
bellclip <legarzaandre@gmail.com>
2014-04-13 11:32:12 -0700

    workshop

--- www/js/app.js
+++ www/js/app.js
@@ -14,17 +14,18 @@
     });
 
     document.addEventListener('deviceready', function () {
-    if (navigator.notification) { // Override default HTML alert with native dialog
-        window.alert = function (message) {
-            navigator.notification.alert(
-                message,    // message
-                null,       // callback
-                "Workshop", // title
-                'OK'        // buttonName
-            );
-        };
-    }
-}, false);
+        FastClick.attach(document.body);
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }, false);
 
 
     /* ---------------------------------- Local Functions ---------------------------------- */
