cddf2595c9095c53be9e1dfd6fdff1e8ea3664d9
bellclip <legarzaandre@gmail.com>
2014-04-13 11:28:48 -0700

    workshop

--- www/js/app.js
+++ www/js/app.js
@@ -13,6 +13,19 @@
         alert("Some help here...")
     });
 
+    document.addEventListener('deviceready', function () {
+    if (navigator.notification) { // Override default HTML alert with native dialog
+        window.alert = function (message) {
+            navigator.notification.alert(
+                message,    // message
+                null,       // callback
+                "Workshop", // title
+                'OK'        // buttonName
+            );
+        };
+    }
+}, false);
+
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
